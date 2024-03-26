5d707837635e61c710dead0e56e2d2f1c52470e7
rry123 <rajy25122@gmail.com>
2024-03-26 13:15:23 +0530

    Completed week7.2

--- week7.2contextapirecoil/week7.2/src/App.jsx
+++ week7.2contextapirecoil/week7.2/src/App.jsx
@@ -1,52 +1,73 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import { useRecoilState, useRecoilValue } from 'recoil'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import { RecoilRoot, useRecoilState, useRecoilValue } from "recoil";
 
-import { countAtom } from './store/atoms/count'
+import { countAtom, evenSelector } from "./store/atoms/count";
+import { outputAtom } from "./store/atoms/output";
 
 function App() {
-  
-  
   return (
     <>
       <div>
-        <Count/>
+        <RecoilRoot>
+          <Count />
+        </RecoilRoot>
       </div>
     </>
-  )
+  );
 }
 
-
-function Count(){
-  return <div>
-    <CountRenderer/>
-    <Buttons/>
-  </div>
+function Count() {
+  return (
+    <div>
+      <CountRenderer />
+      <Buttons />
+    </div>
+  );
 }
-function CountRenderer(){
-  const count = useRecoilValue(countAtom);      // if i need only the value then i will use the useRecoilvalue
+function CountRenderer() {
+  const count = useRecoilValue(countAtom); // if i need only the value then i will use the useRecoilvalue
 
   // if i need to use both count and setCount then use useRecoilState
+  return <div>{count}
+  <EvenCountRenderer></EvenCountRenderer>
+  </div>;
+}
+
+function EvenCountRenderer(){
+  const isEven = useRecoilValue(evenSelector);
+
   return <div>
-    {count}
+    {isEven ? "It is even" : null}
   </div>
 }
 
-function Buttons(){
 
+function Buttons() {
   // Here it needs both count and setCount so i need to use the useRecoilState
   const [count, setCount] = useRecoilState(countAtom);
-
-  return <div>
-    <button onClick={()=>{
-       setCount(count+1);
-    }}>Increase</button>
-    <button onClick={()=>{
-      setCount(count+1);
-    }}>Decrease</button>
-  </div>
+  return (
+    <div>
+      <button
+        onClick={() => {
+          setCount(count + 1);
+        }}
+      >
+        Increase
+      </button>
+      <button
+        onClick={() => {
+          setCount(count - 1);
+        }}
+      >
+        Decrease
+      </button>
+    </div>
+  );
 }
 
-export default App
+
+
+export default App;
---
+++ week7.2contextapirecoil/week7.2/src/store/atoms/output.jsx
@@ -0,0 +1,6 @@
+import { atom } from "recoil";
+
+export const outputAtom = atom({
+    key : "outputAtom",
+    default : "It is Even"
+})
